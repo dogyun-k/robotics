c8c4b92926fca29feb7ed22d2dfeb86c6b1ef8c1
dogyun-k <kdk124578@naver.com>
2020-09-14 13:18:52 +0900

    문제점 발견

--- create_map.py
+++ create_map.py
@@ -18,6 +18,8 @@ class Node:
         self.fire = False
         self.gass = False
 
+        self.recheck = False
+
         # BFS 쓰기위한 변수
         self.state = 0  # 불 : F | 연기 : G | 출구 : E
 
@@ -128,6 +130,16 @@ class Map:
         for node in self.sensor_map.values():
             node.visited = False
 
+    def set_direction_of_exit_false(self, nodes):
+        """입력받은 노드set의 출구방향 False로 설정"""
+        for node in nodes.values():
+            for direction in node.direction_of_exit.keys():
+                node.direction_of_exit[direction] = False
+
+    def set_recheck_false(self, nodes):
+        for i in nodes.values():
+            i.recheck = False
+
     def set_distance_dijkstra(self):
         """다익스트라 알고리즘으로 노드의 distance 설정"""
         for start_num in self.exit_node:
@@ -156,16 +168,18 @@ class Map:
                         heapq.heappush(queue, (adjacent_node.distance, adjacent_node.data))
                         adjacent_node.visited = True
 
-    def direction_of_exit(self, search_node = None):
+    def direction_of_exit(self, search_node = None):    # 넘겨주는 값 : re_set_distance 리턴값
         """노드에서 어느방향으로 탈출해야 할 지 set"""
         if search_node is None:
             search_node = self.sensor_map
 
+        self.set_direction_of_exit_false(search_node)
+
         for current_num, current_node in search_node.items():   # 현재 노드
 
-            for direction, adjacent_node in current_node.adjacent_node.items(): # 현재 노드의 주변 노드
+            for direction, adjacent_node in self.adjacent_node_is(current_node).items(): # 현재 노드의 주변 노드
 
-                if adjacent_node is not None and adjacent_node.distance < current_node.distance:
+                if adjacent_node.distance < current_node.distance:
                     current_node.direction_of_exit[direction] = True
 
     def re_set_distance(self):
@@ -174,27 +188,51 @@ class Map:
         checked_node = {}
 
         # 불과 인접한 노드
-        fire_ad_node = deque()
+        fire_ad_node = [] # 우선순위 큐
 
         for current_node in self.fire_node.values():
             for node in self.adjacent_node_is(current_node).values():
-                if node.fire is False:         
-                    fire_ad_node.append(node)
+                if node.fire is False:
+                    heapq.heappush(fire_ad_node, (node.distance, node.data))   
+                    # fire_ad_node.append(node)
 
         while fire_ad_node:
-            node = fire_ad_node.popleft()
-            for direction, ad_node in self.adjacent_node_is(node).items():
-                if node.distance - ad_node.distance <= -1:
-                    node.distance += 2 * node.weight[direction]
-                    fire_ad_node.append(ad_node)
-                    checked_node[node.data] = node
+            for i in fire_ad_node:
+                print("큐 안에 있는 노드", i[1], "distance", self.sensor_map[i[1]].distance)
+            
+            node_num = heapq.heappop(fire_ad_node)[1]
+            node = self.sensor_map[node_num]
+            print("노드", node.data, "을 꺼냄")
+            
+            # node = fire_ad_node.popleft()
+            node.recheck = True
+            checked_node[node.data] = node
+            ad_nodes = self.adjacent_node_is(node)
 
+
+            for direction, ad_node in ad_nodes.items():
+                print("주변노드", ad_node.data, "distance", ad_node.distance)
+                
+                if node.distance < ad_node.distance and ad_node.recheck is False and ad_node.fire is False:
+                    checked_node[ad_node.data] = ad_node
+                    print("검사할 노드", ad_node.data, "distance", ad_node.distance)
+                    if ad_node.data not in fire_ad_node:
+                        heapq.heappush(fire_ad_node, (ad_node.distance, ad_node.data))  
+                        # fire_ad_node.append(ad_node)
+
+                    node.distance += 2*node.weight[direction]
+            
+            print()
+
+        print(checked_node.keys())
+        self.set_recheck_false(checked_node)
         return checked_node
 
     def set_fire(self, sensor_num):
         """센서에서 불 감지 시 실행"""
         self.sensor_map[sensor_num].state = 'F'
         self.sensor_map[sensor_num].fire = True
+        self.sensor_map[sensor_num].recheck = True      # re_set_distance에 필요
         self.sensor_map[sensor_num].distance = float('inf')
         self.fire_node[sensor_num] = self.sensor_map[sensor_num]
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from create_map import *
 import time
-
+start = time.time()
 
 def print_state(sensor_map):
-    """모든 노드의 상태 값 출력"""
+    """노드set의 상태 값 출력"""
 
     for key, value in sorted(sensor_map.items()):
         direction_list = []
@@ -23,31 +23,49 @@ def print_state(sensor_map):
 
 
 def print_all_adjacent_node(sensor_map):
-    """모든 노드의 인접노드 출력"""
+    """노드set의 인접노드 출력"""
     for key in sensor_map:
         print(key, adjacent_node_is(sensor_map[key]).keys())
 
 
 
-start = time.time()
 
 """ 테스트 라인 """
 print('\n\n')
 knu = Map()
 knu.create_sensor_map('length.txt', 'width.txt', 'stairs.txt', 'exit.txt')
 
+
+
+
 """초기 Distance 설정 (불이 안 난 상황)"""
 knu.set_distance_dijkstra()
 knu.direction_of_exit()
+print_state(knu.sensor_map)
+
+
 
 
 """화재 감지"""
-knu.set_fire('2203')
-knu.re_set_distance()   # 불을 감지하면 다시 distance 설정하고
-knu.direction_of_exit(knu.re_set_distance()) # 노드 방향 다시 설정
+print("\n\n\n\n\n화재 감지 : 19번\n")
+knu.set_fire('19')
+temp = knu.re_set_distance()   # 불을 감지하면 다시 distance 설정하고
+knu.direction_of_exit(temp) # 노드 방향 다시 설정
+print_state(temp)
 
-print_state(knu.sensor_map)
+print("\n\n\n\n\n화재 감지 : 18번\n")
+knu.set_fire('18')
+temp = knu.re_set_distance()   # 불을 감지하면 다시 distance 설정하고
+knu.direction_of_exit(temp) # 노드 방향 다시 설정
+print_state(temp)
 
-print("출구노드 :", knu.exit_node.keys(), "\n")
+print("\n\n\n\n\n화재 감지 : 20번\n")
+knu.set_fire('20')
+temp = knu.re_set_distance()   # 불을 감지하면 다시 distance 설정하고
+knu.direction_of_exit(temp) # 노드 방향 다시 설정
+print_state(temp)
+
+
+
 
 print(f"\n\nset_state 걸린시간 : {time.time() - start}")
